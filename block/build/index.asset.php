4aa279ecb81c942d084224a20102a96811d0341d
jonasroussel <go.jroussel@gmail.com>
2023-12-07 11:19:00 +0100

    Update dependencies and version in index.asset.php file

--- block/build/index.asset.php
+++ block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '1c4f7728315dac6168ca');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '08a509613d47885d3cd0');
--- includes/admin-menu.php
+++ includes/admin-menu.php
@@ -5,12 +5,12 @@ function notice_admin_menu()
 ?>
 	<iframe id="notice-app" src="https://app.notice.studio/editor?_iframe=wordpress" style="width: calc(100% - 20px); height: calc(100svh - 121px); border: none; margin-top: 20px; box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;"></iframe>
 
-	<script>
+	<!-- <script>
 		const iframe = document.getElementById('notice-app')
 
 		iframe.addEventListener('load', (e) => {
 			console.log('Loaded!')
 		})
-	</script>
+	</script> -->
 <?php
 }
